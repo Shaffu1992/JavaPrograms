9af5713415639aa441032a35e127ec312203d38c
shaffu1992 <124557146+Shaffu1992@users.noreply.github.com>
2025-04-03 21:44:08 +0530

    Update FirstJavaProgram.java

--- src/day1/FirstJavaProgram.java
+++ src/day1/FirstJavaProgram.java
@@ -4,7 +4,7 @@ public class FirstJavaProgram {
 	
 	public static void main(String args[])
 	{
-//		System.out.println("welcome to java");
+		System.out.println("welcome to java");
 //		System.out.println(10+20);
 		System.out.println("first committt");
 		System.out.println("Hello World");
