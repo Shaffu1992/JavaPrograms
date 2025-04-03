afe6dcfdfd67bb08adcc7b30c4059dfc4fdd94dc
shaffu_mehta <mehta.shaffu1992@gmail.com>
2025-04-03 21:49:01 +0530

    helooo worlddd

--- src/day1/FirstJavaProgram.java
+++ src/day1/FirstJavaProgram.java
@@ -5,9 +5,10 @@ public class FirstJavaProgram {
 	public static void main(String args[])
 	{
 		System.out.println("welcome to java");
-//		System.out.println(10+20);
+		System.out.println(10+20);
 		System.out.println("first committt");
 		System.out.println("Hello World");
+		System.out.println("Hello World11");
 	}
 	
 	
